f5ef9b01f8e8d21327ca42f7f55df856f0e392ca
Ede Sufiatti <C:/Users/edemi>
2025-03-21 10:00:43 -0300

    c4

--- src/shared/model/users.ts
+++ src/shared/model/users.ts
@@ -1,10 +1,6 @@
 export type User = {
-<<<<<<< HEAD
-  id: number;
-=======
-  id?: string;         // UUID - opcional caso seja gerado pelo banco
->>>>>>> b43d1a9affa2f56a694584a37e22e2b845cb2641
-  name: string;
+ id: number;
+ name: string;
   phone: string;
   email: string;
 };
